da15010c05ee9d88252431169bebe2ea2cc210e5
TamirisKim <kimbaevat@gmail.com>
2023-02-08 16:36:12 +0600

    #4 vuex получи товары из API

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -1,66 +1,51 @@
 <template>
   <div class="v-catalog">
     <v-catalog-item
-    v-for="product in products"
-   :key="product.article"
-  :product_data="product"
-  @sendArticle="showChaldArticle"
-
+      v-for="product in PRODUCTS"
+      :key="product.article"
+      :product_data="product"
+      @sendArticle="showChaldArticle"
     />
   </div>
 </template>
 
 <script>
 import vCatalogItem from "./v-catalog-item";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "v-catalog",
   components: { vCatalogItem },
-  props:{},
-  data(){
-   return{
-      products: [
-     {
-       image: "1.jpg",
-       name: "T-shirt 1",
-       price: 2100.234234234,
-       article: "T1",
-       available: true,
-       category: "Мужские"
-     },
-     {
-       image: "2.jpg",
-       name: "T-shirt 2",
-       price: 3150.12312412,
-       article: "T2",
-       available: true,
-       category: "Женские"
-     },
-     {
-       image: "3.jpg",
-       name: "T-shirt 3",
-       price: 4200.51524,
-       article: "T3",
-       available: false,
-       category: "Женские"
-     },
-   ]
-   }
+  props: {},
+  data() {
+    return {};
   },
-  methods:{
-   showChaldArticle(data){
-      console.log(data)
-   }
-  }
+  computed: {
+    ...mapGetters(["PRODUCTS"]),
+  },
+  methods: {
+    ...mapActions(["GET_PRODUCTS_FROM_API"]),
+
+    showChaldArticle(data) {
+      console.log(data);
+    },
+  },
+  mounted() {
+    this.GET_PRODUCTS_FROM_API()
+    .then((response) => {
+          if (response.data) {
+            console.log('response')
+          }
+        })
+  },
 };
 </script>
 
 <style lang="scss">
 .v-catalog {
-   display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    gap: 20px;
-    align-items: flex-start;
-
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 20px;
+  align-items: flex-start;
 }
 </style>
